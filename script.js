62ef787ede02a8bfa3b20433fe2a3f174557adde
Terin Sebastin <terin01@gmail.com>
2022-11-19 11:04:12 +0000

    Displaying a map marker

--- script.js
+++ script.js
@@ -24,11 +24,22 @@ if (navigator.geolocation) {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
-
-      L.marker(cords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('WorkOut')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position');
